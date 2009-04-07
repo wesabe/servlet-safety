e4aefe8c544ef50943d9166944b973092192feb3
Coda Hale <coda.hale@gmail.com>
2009-04-07 11:51:56 -0700

    Moved SchemeNormalizer over to CaseInsensitiveSet#lowercase.

--- src/main/java/com/wesabe/servlet/normalizers/SchemeNormalizer.java
+++ src/main/java/com/wesabe/servlet/normalizers/SchemeNormalizer.java
@@ -11,13 +11,12 @@ import com.wesabe.servlet.normalizers.util.CaseInsensitiveSet;
  *
  */
 public class SchemeNormalizer implements Normalizer<String> {
-	private final Locale SCHEME_LOCALE = Locale.US;
-	private final CaseInsensitiveSet SCHEMES = CaseInsensitiveSet.of(SCHEME_LOCALE, "HTTP", "HTTPS");
+	private final CaseInsensitiveSet SCHEMES = CaseInsensitiveSet.of(Locale.US, "HTTP", "HTTPS");
 	
 	@Override
 	public String normalize(String scheme) throws ValidationException {
 		if (SCHEMES.contains(scheme)) {
-			return scheme.toLowerCase(SCHEME_LOCALE);
+			return SCHEMES.lowercase(scheme);
 		}
 		
 		throw new ValidationException(scheme, "not a member of " + SCHEMES);
