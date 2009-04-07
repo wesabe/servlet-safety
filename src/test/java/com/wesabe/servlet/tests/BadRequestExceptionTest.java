0a4504749d0fa26c82b3744f5d726ca5b93d0bf3
Coda Hale <coda.hale@gmail.com>
2009-04-06 19:26:36 -0700

    Added BadRequestException.

---
+++ src/test/java/com/wesabe/servlet/tests/BadRequestExceptionTest.java
@@ -0,0 +1,48 @@
+package com.wesabe.servlet.tests;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+import com.wesabe.servlet.BadRequestException;
+
+@RunWith(Enclosed.class)
+public class BadRequestExceptionTest {
+	public static class Throwing_A_Bad_Request_Exception {
+		private Exception cause;
+		private HttpServletRequest request;
+		private BadRequestException exception;
+
+		@Before
+		public void setup() throws Exception {
+			this.request = mock(HttpServletRequest.class);
+			when(request.getMethod()).thenReturn("POST");
+			when(request.getRequestURI()).thenReturn("/woohoo");
+			when(request.getRemoteAddr()).thenReturn("1.2.3.4");
+			
+			this.cause = new Exception("on fire");
+			this.exception = new BadRequestException(request, cause);
+		}
+		
+		@Test
+		public void itHasACause() throws Exception {
+			assertEquals(cause, exception.getCause());
+		}
+		
+		@Test
+		public void itHasAMessage() throws Exception {
+			assertEquals("Bad request: POST to /woohoo from 1.2.3.4", exception.getMessage());
+		}
+		
+		@Test
+		public void itHasABadRequest() throws Exception {
+			assertSame(request, exception.getBadRequest());
+		}
+	}
+}
